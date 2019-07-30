5434115a725a6ac630d38a24a23f8f4bc941234d
mathieuflemmingite <matdrillet@gmail.com>
2019-07-30 09:41:23 +0200

    Add files via upload

---
+++ exo_AWS.ps1
@@ -0,0 +1,34 @@
+﻿
+
+#ssh-keygen -t rsa -b 2048 -f $HOME\.ssh\id_rsa 
+
+$sshPublicKey = Get-Content "$HOME\.ssh\id_rsa.pub"
+
+New-AzResourceGroup -Name 'testressources' -Location 'westeurope'
+
+for ($i=0;$i -le 2;$i++)
+{
+
+    
+    $nameVM = Read-Host "Donnez un nom à votre VM : "
+
+    $parameters = @{
+      ResourceGroupName = 'testressources'
+      Name = $nameVM
+      Location = 'westeurope'
+      ImageName = 'UbuntuLTS'
+      Credential = $credential
+      KeyData = $sshPublicKey
+      OpenPorts = 22
+    }
+    
+    $newVM1 = New-AzVM @parameters
+
+    $publicIp = Get-AsPublicIpAdress -Name $nameVM
+
+    ssh $env:USERNAME@publicIP
+
+    git clone git@vps706123.ovh.net:root/boite-a-outils.git
+
+}
+
